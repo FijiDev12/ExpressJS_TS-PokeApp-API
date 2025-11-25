7f6dbd06ca55173e81a3242efebad375a982ba1b
FijiGVI <carljerson.oquila@globalvisionsinc.com.ph>
2025-11-25 08:34:53 +0800

    new repo

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,26 @@
 import dotenv from 'dotenv';
-dotenv.config(); // MUST be first, before any other imports
+dotenv.config(); // MUST be first
 
 import app from './app';
 import fs from 'fs';
 import https from 'https';
 
-const PORT = process.env.PORT || 8502;
+// Always convert to number
+const PORT = Number(process.env.PORT) || 8502;
 
-// Optional HTTPS setup
+// Listen on all network interfaces
+const HOST = "0.0.0.0";
+
+
+// OPTIONAL: HTTPS
 // const sslOptions = {
-//     key: fs.readFileSync('/etc/letsencrypt/archive/api-psp-ext.festivalcasino.com/privkey2.pem'),
-//     cert: fs.readFileSync('/etc/letsencrypt/archive/api-psp-ext.festivalcasino.com/fullchain2.pem')
-// }
+//     key: fs.readFileSync('/etc/letsencrypt/archive/api-dev-pmt.com/privkey2.pem'),
+//     cert: fs.readFileSync('/etc/letsencrypt/archive/api-dev-pmt.com/fullchain2.pem')
+// };
 
-// https.createServer(sslOptions, app).listen(PORT, () => {
-//     console.log(`🚀 Server running on https://0.0.0.0:${PORT}`);
-// });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port http://0.0.0.0:${PORT}`);
+app.listen(PORT, HOST, () => {
+    console.log(`🚀 API Server running at:`);
+    console.log(`➡ Local Network:   http://10.20.10.114:${PORT}`);
+    // console.log(`➡ Public (WAN):    http://112.198.178.39:${PORT} (requires port forwarding)`);
 });
